7e1e9300498025249dca101781da41cb71c0ac65
Melissa-mon <71413547+Melissa-mon@users.noreply.github.com>
2025-01-27 21:43:05 -0500

    ...

--- src/gestorAplicacion/Servicios/Habitacion.java
+++ src/gestorAplicacion/Servicios/Habitacion.java
@@ -1,66 +1,105 @@
-package gestorAplicacion.Servicios;
+package gestorAplicacion.habitaciones;
 
-public class Habitacion {
-    // Atributos
+import java.io.Serializable;
+
+public class Habitacion implements Serializable {
+    private static final long serialVersionUID = 1L; // Identificador único para la serialización
+
+    private int numero;
+    private double precioBase;
+    private String tipo;
+    private String vista;
+    private String capacidad;
+    private boolean servicioHabitacion;
+    private boolean sucia;
+    private double descuento;
     private boolean ocupada;
-    private int NumeroCamas;
-    private int NumeroNoches;
-    private String tipoHabitacion;
-    private boolean habitacionLimpia;
-    // Constructor vacio
-    public Habitacion() {
-    }
-    // Constructor con argumentos
-    public Habitacion(boolean ocupada, int NumeroCamas, int NumeroNoches, String tipoHabitacion, boolean habitacionLimpia) {
+    private int nochesOcupadas;
+
+    public Habitacion(int numero, double precioBase, String tipo, String vista, String capacidad,
+            boolean servicioHabitacion, boolean sucia, double descuento, boolean ocupada) {
+        this.numero = numero;
+        this.precioBase = precioBase;
+        this.tipo = tipo;
+        this.vista = vista;
+        this.capacidad = capacidad;
+        this.servicioHabitacion = servicioHabitacion;
+        this.sucia = sucia;
+        this.descuento = descuento;
         this.ocupada = ocupada;
-        this.NumeroCamas = NumeroCamas;
-        this.NumeroNoches = NumeroNoches;
-        this.tipoHabitacion = tipoHabitacion;
-        this.habitacionLimpia = habitacionLimpia;
-    }
-    
-     // Getters y Setters
-    public boolean getOcupada() {
-        return this.ocupada;
+        this.nochesOcupadas = 0;
     }
 
-    public void setOcupada(boolean ocuapada) {
-        this.ocupada = ocuapada;
+    public int getNumero() {
+        return numero;
     }
 
-    public int getNumeroCamas() {
-        return this.NumeroCamas;
+    public double getPrecioBase() {
+        return precioBase;
     }
 
-    public void setNumeroCamas(int NumeroCamas) {
-        this.NumeroCamas = NumeroCamas;
+    public String getTipo() {
+        return tipo;
     }
 
-    public int getNumeroNoches() {
-        return this.NumeroNoches;
+    public String getVista() {
+        return vista;
     }
 
-    public void setNumeroNoches(int NumeroNoches) {
-        this.NumeroNoches = NumeroNoches;
+    public String getCapacidad() {
+        return capacidad;
     }
 
-    public String getTipoHabitacion() {
-        return this.tipoHabitacion;
+    public boolean isServicioHabitacion() {
+        return servicioHabitacion;
     }
 
-    public void setTipoHabitacion(String tipoHabitacion) {
-        this.tipoHabitacion = tipoHabitacion;
+    public boolean isSucia() {
+        return sucia;
     }
 
-    public boolean isHabitacionLimpia() {
-        return this.habitacionLimpia;
+    public double getDescuento() {
+        return descuento;
     }
 
-    public boolean getHabitacionLimpia() {
-        return this.habitacionLimpia;
+    public boolean isOcupada() {
+        return ocupada;
     }
 
-    public void setHabitacionLimpia(boolean habitacionLimpia) {
-        this.habitacionLimpia = habitacionLimpia;
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" +
+                "numero=" + numero +
+                ", tipo='" + tipo + '\'' +
+                ", vista='" + vista + '\'' +
+                ", capacidad='" + capacidad + '\'' +
+                ", servicioHabitacion=" + servicioHabitacion +
+                ", sucia=" + sucia +
+                ", precioBase=" + precioBase +
+                ", descuento=" + descuento +
+                ", ocupada=" + ocupada +
+                '}';
+    }
+
+    public double calcularPrecioConDescuento(double v) {
+        return precioBase - (precioBase * descuento / 100);
+    }
+
+    public void limpiar() {
+        sucia = false; // La habitación se limpia
+    }
+
+    public void verificarEstadoSucia() {
+        // La habitación se considera sucia si su número es par y no es cero
+        sucia = (nochesOcupadas % 2 == 0 && nochesOcupadas != 0);
+    }
+
+    public void incrementarNochesOcupadas() {
+        nochesOcupadas++;
+
     }
 }
